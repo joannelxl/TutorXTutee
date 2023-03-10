7ee020286481c02619b77d4846f73ad195709553
Joanne <joannelimxiangling@gmail.com>
2023-03-10 19:44:10 +0800

    movable sidebar implemented without links

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+// import router from "./routers/index.js";
 
-import './assets/main.css'
+import "@fortawesome/fontawesome-free/js/all";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
