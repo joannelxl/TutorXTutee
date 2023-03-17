d94854ebddcceb22b16ea80d5fa30cd2640ce84d
joannelxl <joannelimxiangling@gmail.com>
2023-03-17 18:42:49 +0800

    move homepage to Home.vue from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import Sidebar from '@/components/sidebar/Sidebar.vue'
 import { sidebarWidth } from '@/components/sidebar/state'
+import Home from './views/Home.vue';
 export default {
     components: { Sidebar },
     setup() {
@@ -12,22 +13,6 @@ export default {
     <Sidebar />
     <div :style="{ 'margin-left': sidebarWidth }">
         <router-view />
-    </div>
-
-    <div class="homepage">
-        <div class="title">
-            <img alt="TxT logo" src="@/assets/homepagePic.jpg" />
-            <p class="welcomeTo">Welcome to</p>
-            <p class="TutorXTutee">Tutor X Tutee</p>
-        </div>
-
-        <div class="aboutUs">
-            <h3>About Us</h3>
-            <p>Tutor x Tutee aims to help tutors better manage and schedule their tutoring sessions with tutees. With this
-                platform, we hope to reduce the barriers to voluntary tutoring and allow tutors to better focus on the
-                tutoring
-                sessions. Tutees will also be able to put up requests anytime they need help.</p>
-        </div>
     </div>
 </template>
 
@@ -52,82 +37,5 @@ export default {
 
 #nav a.router-link-exact-active {
     color: #620562;
-}
-</style>
-/* for homepage styling */
-<style scoped>
-.homepage {
-    background-color: #ca95ca;
-}
-
-.title .welcomeTo {
-    position: absolute;
-    top: 30px;
-    left: 300px;
-    font-weight: 400;
-    font-size: 70px;
-    line-height: 55px;
-    margin-top: 0px;
-    /* identical to box height */
-
-    color: rgba(128, 0, 128, 0.66);
-
-    border: 1px solid #FFFFFF;
-}
-
-.TutorXTutee {
-    top: 35px;
-    position: absolute;
-    width: 607px;
-    height: 0px;
-    left: 700px;
-    font-weight: 400;
-    font-size: 100px;
-    line-height: 78px;
-    /* identical to box height */
-
-
-    color: rgba(128, 0, 128, 0.66);
-
-    border: 1px solid #FFFFFF;
-}
-
-.title img {
-    position: absolute;
-    width: 46%;
-    height: auto;
-    max-height: 600px;
-    left: 150px;
-    top: 250px;
-    border: 1px solid #000000;
-    backdrop-filter: blur(2px);
-}
-
-.aboutUs h3 {
-    position: absolute;
-    width: 140px;
-    height: 66px;
-    left: 1050px;
-    top: 250px;
-    font-weight: bold;
-    font-weight: 400;
-    font-size: 30px;
-    line-height: 23px;
-
-    color: #000000;
-}
-
-.aboutUs p {
-    position: absolute;
-    width: 405px;
-    height: 438px;
-    left: 900px;
-    top: 300px;
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 50px;
-
-    color: #000000;
-
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,88 @@
-<template></template>
+<template>
+    <div class="homepage">
+        <div class="title">
+
+            <p class="welcomeTo">Welcome to</p>
+            <p class="TutorXTutee">Tutor X Tutee</p>
+            <img alt="TxT logo" src="@/assets/homepagePic.jpg" />
+
+        </div>
+
+        <div class="aboutUs">
+            <h3>About Us</h3>
+            <p>Tutor x Tutee aims to help tutors better manage and schedule their tutoring sessions with tutees. With this
+                platform, we hope to reduce the barriers to voluntary tutoring and allow tutors to better focus on the
+                tutoring
+                sessions. Tutees will also be able to put up requests anytime they need help.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { collapsed, toggleSidebar } from '../components/sidebar/state';
+
+export default {
+    name: "Home",
+    setup() {
+        return { collapsed, toggleSidebar }
+    }
+}
+</script>
+
+<style scoped>
+.title .welcomeTo {
+    font-weight: 400px;
+    width: 40%;
+    font-size: 70px;
+    line-height: 55px;
+    margin-top: 3%;
+    color: rgba(128, 0, 128, 0.66);
+    border: 1px solid #FFFFFF;
+    margin-bottom: 2%;
+
+}
+
+.TutorXTutee {
+    width: 80%;
+    height: 0px;
+    margin-top: 3%;
+    font-weight: 400;
+    font-size: 100px;
+    line-height: 78px;
+    color: rgba(128, 0, 128, 0.66);
+    border: 1px solid #FFFFFF;
+}
+
+.title img {
+    width: 45%;
+    float: left;
+    margin-left: 5%;
+    height: auto;
+    max-height: 10%;
+    border: 1px solid #000000;
+    margin-top: 2%;
+}
+
+.aboutUs h3 {
+    float: right;
+    margin-right: 16%;
+    width: 20%;
+    height: 2%;
+    font-weight: 400px;
+    font-size: 30px;
+    line-height: 5%;
+    color: #000000;
+}
+
+.aboutUs p {
+    float: right;
+    margin-right: 10%;
+    width: 32%;
+    top: 1%;
+    font-weight: 400px;
+    font-size: 20px;
+    line-height: 200%;
+    color: #000000;
+
+}
+</style>
