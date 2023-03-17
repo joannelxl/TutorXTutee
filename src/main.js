5889d4649ac62b78b945d209721593db89aaeb9f
joannelxl <joannelimxiangling@gmail.com>
2023-03-17 18:43:13 +0800

    comment out vuetify code

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "@fortawesome/fontawesome-free/js/all";
-import router from "./router";
+import router from "./routers";
+
+// // Vuetify
+// import "vuetify/styles";
+// import { createVuetify } from "vuetify";
+// import * as components from "vuetify/components";
+// import * as directives from "vuetify/directives";
+
+// const vuetify = createVuetify({
+//   components,
+//   directives,
+// });
 
 createApp(App).use(router).mount("#app");
