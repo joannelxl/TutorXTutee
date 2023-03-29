636fa3ab459b86c045368c78fbbd7fe0d0f4e25a
joannelxl <joannelimxiangling@gmail.com>
2023-03-29 23:26:43 +0800

    import display component for both tutor and tutee

--- src/views/MyTutors.vue
+++ src/views/MyTutors.vue
@@ -1,1 +1,29 @@
-<template></template>
+<template>
+    <DisplayProgress :key="refreshComp" />
+    {{ id }}
+</template>
+  
+<script>
+import DisplayProgress from "../components/DisplayProgress.vue";
+export default {
+    components: {
+        DisplayProgress,
+    },
+    data() {
+        return {
+            refreshComp: 0,
+            id: null,
+        };
+    },
+    methods: {
+        change() {
+            this.refreshComp += 1;
+        },
+    },
+    mounted() {
+        this.id = this.$route.params.id;
+    }
+};
+</script>
+  
+<style></style>
