e3367fd110eedf5d57c90bd6dc44a689407ac9e9
joannelxl <joannelimxiangling@gmail.com>
2023-03-31 00:13:07 +0800

    edit components

--- src/components/DisplayProgress.vue
+++ src/components/DisplayProgress.vue
@@ -1,39 +1,49 @@
 <template>
-    <div v-if="role == 'Tutor'">
+    <!-- Back to tutoring arrangement  -->
+    <div v-if="role == 'tutor'">
+        <router-link to="/MyTutees" style="font-family: Arial, Helvetica, sans-serif">Back to MyTutees</router-link>
         <button id="show-modal" @click="navigateProgressNotes"></button>
+    </div>
+
+    <div v-if="role == 'tutee'">
+        <router-link to="/MyTutors" style="font-family: Arial, Helvetica, sans-serif">Back to MyTutors</router-link>
     </div>
 
     <div class="main-container">
         <h1>Tutor X Tutee</h1>
-        <h4>All your written progress are listed here.</h4>
-
-        <!-- if tutor -->
-        <h1 class="empty" v-if="progressNotes.length == 0 && role == 'Tutor'">
-            You do not have any progress notes now. To add please click the + button.
-        </h1>
+        <h4>All your written progress for {{ tuteeName }} are listed here.</h4>
 
         <!-- if tutee -->
-        <h1 class="empty" v-if="progressNotes.length == 0 && role == 'Tutee'">
-            Your tutor have not added any progress note. Progress Notes will be displayed here once he/she writes one.
-        </h1>
+        <div class="empty" v-if="progressNotes.length == 0 && role == 'tutee'">
+            <h3>Your tutor have not added any progress note.</h3>
+            <h3>Progress Notes will be displayed here once he/she writes one.</h3>
+        </div>
+
+        <!-- if tutee -->
+        <div class="empty" v-if="progressNotes.length == 0 && role == 'tutor'">
+            <h3>You do not have any progress notes now. </h3>
+            <h3>To add please click the + button.</h3>
+        </div>
 
         <div class="progress">
             <div class="req" v-for="note in progressNotes">
                 <div class="card">
                     <div class="container">
-                        <p class="date"> {{ note[1].Date.toDate().toLocaleDateString() }}</p>
+                        <p class="date"> {{ note[1].Date.toDate().toLocaleDateString("en-GB") }}</p>
                         <p class="lesson"><strong>Lesson {{ note[1].Lesson }}</strong></p>
 
                     </div>
                     <br><br>
-                    <button v-if="role == 'Tutor'" class="delete-button" @click="handleDelete(note[0])">
-                        Delete Note
-                    </button>
                     <p class="remarks">
                         {{ note[1].Remarks }}
+                        <br><br>
+                        <button v-if="role == 'tutor'" class="delete-button" @click="handleDelete(note[0])">
+                            Delete Note
+                        </button>
                     </p>
 
                 </div>
+
                 <br>
             </div>
         </div>
@@ -42,9 +52,7 @@
 
 <script>
 import firebaseApp from "../firebase.js";
-import { deleteDoc, getFirestore } from "firebase/firestore";
-
-
+import { deleteDoc, getFirestore, getDoc } from "firebase/firestore";
 import {
     collection,
     getDocs,
@@ -53,31 +61,37 @@ import {
     doc,
     orderBy,
 } from "firebase/firestore";
+
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 const db = getFirestore(firebaseApp);
 export default {
+    props: {
+        id: String
+    },
     data() {
         return {
             progressNotes: [],
             useremail: "",
-            //to be filled up 
-            role: 'Tutor',
+            role: '',
+            tuteeName: '',
+            id: this.id,
         };
     },
-
     methods: {
         navigateProgressNotes() {
-            this.$router.push({ path: '/ProgressNotes' })
+            this.$router.push({ name: "AddProgress", params: { id: this.id } })
         },
         async display() {
             this.progressNotes = [];
             const progressRef = collection(db, "ProgressNotes");
-            const q = query(progressRef, orderBy("Lesson", 'desc'));
+            const q = query(progressRef, orderBy("Lesson", 'desc'), where("Id", "==", this.id));
             const querySnapshot = await getDocs(q);
             console.log("displayed");
             querySnapshot.forEach((doc) => {
                 this.progressNotes.push([doc.id, doc.data()]);
             });
+            console.log(this.id)
         },
         async handleDelete(requestId) {
             await deleteDoc(doc(db, "ProgressNotes", requestId));
@@ -87,12 +101,29 @@ export default {
     },
     async mounted() {
         const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
+        onAuthStateChanged(auth, async (user) => {
             if (user) {
+                //to obtain role
                 this.useremail = user.email;
+                const verifiedUser = await getDoc(doc(db, "VerifiedUsers", this.useremail));
+                this.role = verifiedUser.data().role
+
+                //display 
                 this.display();
+
+                //to get tutee name
+                const docRef = doc(db, "TutoringArrangements", this.id);
+                try {
+                    const docSnap = await getDoc(docRef);
+                    var account = await getDoc(doc(db, "Tutees", docSnap.data().tuteeEmail))
+                    this.tuteeName = account.data().firstName + " " + account.data().lastName
+                } catch (error) {
+                    console.log(error)
+                }
+                this.dataLoaded = true
+
             }
-        });
+        })
     },
 };
 </script>
@@ -110,7 +141,11 @@ export default {
 }
 
 .empty {
-    padding-top: 200px;
+    background-color: #f3ddb0;
+    padding: 5px 10px;
+    border-radius: 10px;
+    margin: 0px;
+
 }
 
 .card {
@@ -121,7 +156,7 @@ export default {
     min-height: 100%;
     background-color: #f3ddb0;
     height: 20%;
-    padding-bottom: 10px;
+    padding-bottom: 2px;
 }
 
 .lesson {
@@ -130,14 +165,18 @@ export default {
 
 .date {
     float: right;
+    margin-right: 15px;
+    font-family: 'Radley';
+    font-weight: bold
 }
 
 .remarks {
 
     overflow-wrap: break-word;
     text-overflow: ellipsis;
-    margin-left: 20px;
+    margin-left: 16px;
     overflow: auto;
+    justify-content: baseline;
 }
 
 .container {
@@ -151,19 +190,24 @@ export default {
 }
 
 .delete-button {
-    background-color: #f7a588;
-    padding: 10px 10px;
-    border-radius: 4px;
+    border-radius: 5px;
+    width: 90px;
+    text-align: left;
+    margin-top: 2px;
+    margin-bottom: 2px;
+    margin-right: 9px;
+    border: none;
+    height: 30px;
     font-size: small;
-    margin-left: -5px;
-    margin-right: 5px;
-    margin-top: 10px;
-    float: right
+    cursor: pointer;
+    box-shadow: 2px 2px gray;
+    background-color: rgba(240, 51, 51, 0.56);
+    float: left
 }
 
 .delete-button:hover {
-    background-color: #c27f66;
     cursor: pointer;
+    background-color: white;
 }
 
 #show-modal {
---
+++ src/views/AddProgress.vue
@@ -0,0 +1,24 @@
+<template>
+    <AddProgressNotes @added="change" />
+</template>
+  
+<script>
+import AddProgressNotes from "../components/AddProgressNotes.vue";
+export default {
+    components: {
+        AddProgressNotes,
+    },
+    data() {
+        return {
+            refreshComp: 0,
+        };
+    },
+    methods: {
+        change() {
+            this.refreshComp += 1;
+        },
+    },
+};
+</script>
+  
+<style></style>
