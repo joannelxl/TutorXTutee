a64dd96cb5df203f92a521ff3a5d9d75a5938958
Teresa <teresasee02@gmail.com>
2023-04-14 12:12:59 +0800

    editted and redeployed

--- src/components/RequestDisplay.vue
+++ src/components/RequestDisplay.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="container" v-if="dataLoaded">
         <h1 class="empty" v-if="requests.length == 0">
-        You do not have any request now. To add please click the + button
+            You do not have any request now. To add please click the + button
         </h1>
         <div class="filterblock" @click="showmodal">
             <b>Filter <i class='fa fa-filter' style='color: lightsteelblue;'></i></b>
         </div>
         <div class="modal-mask" v-if="showModal">
             <div class="modal-container">
-            <form @submit.prevent="">
+                <form @submit.prevent="">
                     <div class="modal-header">
                         <div class="close" @click="handleClose" style="font-size: large;">
                             <b>&times;</b>
                         </div>
                         <div style="font-size: larger;"><b>Filter based on your preferences</b></div>
-                    </div><br> 
+                    </div><br>
                     <div class="modal-body">
                         <div class="block">
                             <b>Location Preferences: </b>
@@ -41,17 +41,20 @@
                     <br>
                     <div class="modal-footer">
                         <div class="buttons">
-                            <button class="close-btn" type="button" @click="handleClose">Cancel</button> &nbsp &nbsp &nbsp &nbsp &nbsp
+                            <button class="close-btn" type="button" @click="handleClose">Cancel</button> &nbsp &nbsp &nbsp
+                            &nbsp &nbsp
                             <button class="update-btn" type="button" v-on:click="updateFilter"> Filter </button><br>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-        <div class = "req-con">
+        <div class="req-con">
             <div class="requests" v-for="request in filteredRequests">
                 <div class="cards" @click="redirectToInfo(request[0])">
-                    <div style="font-size: larger;"><p><b> {{ request[2] }} </b></p></div>
+                    <div style="font-size: larger;">
+                        <p><b> {{ request[2] }} </b></p>
+                    </div>
                     <p><b>Level:</b> {{ request[1].Level }}</p>
                     <p><b>Subject:</b> {{ request[1].Subject }}</p>
                     <p><b>Location:</b> {{ request[1].Location }}</p>
@@ -59,10 +62,9 @@
             </div>
         </div><br>
         <div class="message" v-if="filteredRequests.length == 0">
-            There are no such listings with your preference. You can consider widening your range!
+            <h2>There are no such listings with your preference. You can consider widening your range!</h2>
         </div>
     </div>
-    
 </template>
 
 <script>
@@ -86,7 +88,7 @@ export default {
             subjects: [],
             levels: [],
             user: null,
-            
+
             selectedLocations: [],
             selectedSubjects: [],
             selectedLevels: [],
@@ -105,7 +107,7 @@ export default {
 
             // for every request in the database
             // request has the following attributes: Address, Level, Location, PreferredDays, PreferredTime, Remarks, Subject, User
-            
+
             if (this.selectedLocations.length == 0) {
                 this.selectedLocations = this.locations
             }
@@ -115,7 +117,7 @@ export default {
             if (this.selectedLevels.length == 0) {
                 this.selectedLevels = this.levels
             }
-            var count2 = 0 
+            var count2 = 0
             querySnapshot.forEach(async (docu) => {
                 var tuteeEmail = docu.data().User;
                 var tutee = await getDoc(doc(db, "Tutees", tuteeEmail))
@@ -132,7 +134,7 @@ export default {
                     this.filteredRequests.push([docu.id, docu.data(), tutee.data().firstName + " " + tutee.data().lastName]);
                 }
                 count2 += 1
-                
+
                 if (count2 == querySnapshot.size) {
                     this.selectedLocations = [];
                     this.selectedSubjects = [];
@@ -140,11 +142,11 @@ export default {
                     this.handleClose();
                     this.dataLoaded = true
                 }
-            })        
+            })
         },
         redirectToInfo(documentID) {
-			this.$router.push({name: "RequestsInfo", params: {id: documentID}})
-		},
+            this.$router.push({ name: "RequestsInfo", params: { id: documentID } })
+        },
         updateFilter() {
             this.display()
         },
@@ -197,7 +199,7 @@ export default {
                 })
                 this.display();
 
-            }  
+            }
         });
     }
 }
@@ -221,11 +223,11 @@ export default {
     overflow-wrap: break-word;
 }
 
-.cards:hover{
-    box-shadow: 7px 7px 3px rgba(0,0,0,0.24);
+.cards:hover {
+    box-shadow: 7px 7px 3px rgba(0, 0, 0, 0.24);
 }
 
-.requests{
+.requests {
     display: inline-block;
     grid-template-columns: auto auto auto;
     text-align: center;
@@ -233,22 +235,22 @@ export default {
     align-items: center;
 }
 
-.req-con{
+.req-con {
     margin-left: 10vh;
     display: grid;
     grid-template-columns: auto auto auto;
 }
 
-.container{
+.container {
     text-align: center;
     width: 90vw;
 }
 
-.filterFunction{
+.filterFunction {
     resize: both;
 }
 
-.modal-mask{
+.modal-mask {
     position: fixed;
     z-index: 9998;
     top: 0;
@@ -270,7 +272,7 @@ export default {
     transition: all 0.3s ease;
 }
 
-.modal-body{
+.modal-body {
     text-align: left;
     margin-left: 12vh;
 }
@@ -284,15 +286,16 @@ input {
     padding-left: 3vh;
 }
 
-.buttons{
+.buttons {
     text-align: center;
     display: block;
     font-size: large;
 }
 
-.close-btn:hover, .update-btn:hover {
+.close-btn:hover,
+.update-btn:hover {
     background-color: #e0dad4;
-    box-shadow: 3px 3px 7px rgba(0,0,0,0.24);
+    box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.24);
     background-color: #C8B6A6;
 }
 
@@ -303,23 +306,28 @@ input {
 }
 
 button {
-	border-radius: 5px;
-	padding: 10px;
-	width: auto;
-	text-align: left;
-	margin-top: 5px;
-	margin-bottom: 5px;
-	border: 1px solid #000000;
-	height: 40px;
-	font-size: large;
-	cursor: pointer;
+    border-radius: 5px;
+    padding: 10px;
+    width: auto;
+    text-align: left;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    border: 1px solid #000000;
+    height: 40px;
+    font-size: large;
+    cursor: pointer;
     background-color: #F1DEC9;
 }
-.filterblock{
+
+.filterblock {
     float: right;
     text-align: right;
     margin-right: 20vh;
     cursor: pointer;
     font-size: larger;
 }
-</style>
+
+.message {
+    text-align: center;
+    margin-top: 200px;
+}</style>
